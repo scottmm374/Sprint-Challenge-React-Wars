79c381070f9b1b946a14786e64a6faf65063f5ea
Michelle Scott <scottmm374@gmail.com>
2019-06-09 20:35:56 -0400

    redid Character.js file

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,29 +1,44 @@
 import React from 'react';
 
-class Character extends React.Component (
-    constructor(props) {
-        super(props) {
-            this.state = {
-                birthYear: "",
-                created: "",
-                edited: "",
-                eyeColor: "",
-                films: "",
-                gender: "",
-                hairColor: "",
-                height: "",
-                homeWorld: "",
-                mass: "",
-                name: "",
-                skinColor: "",
-                species: "",
-                starShips: "",
-                url: "",
-                vehicles: "",
-
-            }
-        }
-    }
-)
-
+function Character(props) {
+    return (
+        <div>
+            <p><strong>created:</strong>{props.starwarsChars.created}</p>
+            <p><strong>edited:</strong>{props.starwarsChars.edited}</p>
+            <p><strong>eyeColor:</strong>{props.starwarsChars.eyeColor}</p>
+            <p><strong>films:</strong>{props.starwarsChars.films}</p>
+            <p><strong>gender:</strong>{props.starwarsChars.gender}</p>
+            <p><strong>hairColor:</strong>{props.starwarsChars.hairColor}</p>
+            <p><strong>height:</strong>{props.starwarsChars.height}</p>
+            <p><strong>homeWorld:</strong>{props.starwarsChars.homeWorld}</p>
+            <p><strong>mass:</strong>{props.starwarsChars.mass}</p>
+            <p><strong>name:</strong>{props.starwarsChars.name}</p>
+            <p><strong>skinColor:</strong>{props.starwarsChars.skinColor}</p>
+            <p><strong>species:</strong>{props.starwarsChars.species}</p>
+            <p><strong>starShips:</strong>{props.starwarsChars.starShips}</p>
+            <p><strong>url:</strong>{props.starwarsChars.url}</p>
+            <p><strong>vehicles:</strong>{props.starwarsChars.vehicles}</p>           
+        </div>
+    );
+}
+   
 export default Character;
+
+
+
+ // birthYear: "",
+                // created: "",
+                // edited: "",
+                // eyeColor: "",
+                // films: "",
+                // gender: "",
+                // hairColor: "",
+                // height: "",
+                // homeWorld: "",
+                // mass: "",
+                // name: "",
+                // skinColor: "",
+                // species: "",
+                // starShips: "",
+                // url: "",
+                // vehicles: "",
--- starwars/src/components/CharactersList.js
+++ starwars/src/components/CharactersList.js
@@ -1,15 +1,19 @@
 import React from "react";
+import Character from "./Character";
 
 function CharactersList (props) {
+    console.log(props)
     return (
-        <div className="character-card">
-            {props.starwarsChars.map(char =>
+        <div>
+            {props.starwarsChars.map(char => {
                 return (
-                    
-                ))}
-
+                    <Character
+                        characterInfo={char}
+                     />   
+                );
+             })}
         </div>
-    )
+    );
 }
 
 
